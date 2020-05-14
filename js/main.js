da2adf9887418db28e8f2ac885f1b8643b27ea96
Grigorevse <63945773+Grigorevse@users.noreply.github.com>
2020-05-14 11:40:44 +0700

    Обновляем яву

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
         jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1000);
         return false;
         });
-});
 
 $('.counter').each(function () {
         $(this).prop('Counter',0).animate({
@@ -17,4 +16,57 @@ $('.counter').each(function () {
         $(this).text(Math.ceil(now));
         }
         });
-      });      
+});  
+
+$('.section-wrapper').slick({
+                accessibility: true,
+                adaptiveHeight: true,
+                arrows: true,
+                asNavFor: null,
+                prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">Previous</button>',
+                nextArrow: '<button class="slick-next" aria-label="Next" type="button">Next</button>',
+                autoplay: false,
+                autoplaySpeed: 3000,
+                centerMode: false,
+                centerPadding: '50px',
+                cssEase: 'ease',
+                dots: true,
+                dotsClass: 'slick-dots',
+                draggable: true,
+                easing: 'linear',
+                edgeFriction: 0.35,
+                fade: false,
+                focusOnSelect: false,
+                focusOnChange: false,
+                infinite: true,
+                initialSlide: 0,
+                lazyLoad: 'ondemand',
+                mobileFirst: false,
+                pauseOnHover: true,
+                pauseOnFocus: true,
+                pauseOnDotsHover: false,
+                respondTo: 'window',
+                responsive: null,
+                rows: 1,
+                rtl: false,
+                slide: '',
+                slidesPerRow: 1,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 500,
+                swipe: true,
+                swipeToSlide: false,
+                touchMove: true,
+                touchThreshold: 5,
+                useCSS: true,
+                useTransform: true,
+                variableWidth: false,
+                vertical: false,
+                verticalSwiping: false,
+                waitForAnimate: true,
+                zIndex: 1000               
+        });
+});
+
+
+    
